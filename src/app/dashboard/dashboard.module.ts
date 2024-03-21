7acff23edd90d14f1a565f34bef1306f563e0754
Tarunkumar-Mylipilli <tarun.developer99@gmail.com>
2024-03-21 21:27:25 +0530

    changes

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { IncomeComponent } from './income/income.component';
+import { ExpenseComponent } from './expense/expense.component';
+import { TodoTransactionsComponent } from './todo-transactions/todo-transactions.component';
+import { AllTransactionsComponent } from './all-transactions/all-transactions.component';
+import { AddBudgetComponent } from './add-budget/add-budget.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    SideNavComponent,
+    IncomeComponent,
+    ExpenseComponent,
+    TodoTransactionsComponent,
+    AllTransactionsComponent,
+    AddBudgetComponent,
+    LoginComponent
   ]
 })
 export class DashboardModule { }
